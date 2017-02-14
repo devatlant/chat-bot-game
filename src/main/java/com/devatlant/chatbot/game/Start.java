66fe02920831c1a1c18ed0aee7273b97f70d4dc5
Voronetskyy Yevgen <yev@devatalnt.com>
2017-02-14 21:38:38 +0200

    set package name

--- src/main/java/com/devatlant/chatbot/game/Start.java
+++ src/main/java/com/devatlant/chatbot/game/Start.java
@@ -1,3 +1,5 @@
+package com.devatlant.chatbot.game;
+
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
