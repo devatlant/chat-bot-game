f6ff525da6beec0d313145fbb60813fd5d7ef462
Voronetskyy Yevgen <yev@devatalnt.com>
2017-02-14 12:35:11 +0200

    initial commit

---
+++ src/main/java/Start.java
@@ -0,0 +1,22 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+/**
+ * Created by yev on 2/10/17.
+ */
+public class Start {
+		public static void main(String[] args) {
+
+			ApiContextInitializer.init();
+
+			TelegramBotsApi botsApi = new TelegramBotsApi();
+
+			try {
+				botsApi.registerBot(new HelloWorldBot());
+			} catch (TelegramApiException e) {
+				e.printStackTrace();
+			}
+		}
+
+}
